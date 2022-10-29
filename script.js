1a153cb90db748bd8f806992bf97a55e1e79aaf6
Diana <kabadiana327@gmail.com>
2022-10-29 17:12:19 +0300

    unsuccessful attempt

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+function factorial(n) {
+    let m = prompt('Input number:');
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n *= m;
+    }
+}
+
+factorial();
